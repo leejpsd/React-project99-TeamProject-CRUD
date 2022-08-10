a992676c0a6a821194eb2db3590053bdb60c2cc8
jungpyo lee <101346918+leejpsd@users.noreply.github.com>
2022-08-11 07:28:20 +0900

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { __getTodos } from "../redux/modules/todos";
-
 import { __deleteTodos } from "../redux/modules/todos";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { timeForToday } from "./Time";
 import Update from "./Update";
+import {
+  __deleteComents,
+  __getComents,
+  __postComents,
+} from "../redux/modules/comments";
 import Comment from "./Comment";
 
 const Home = () => {
@@ -15,80 +19,94 @@ const Home = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const ComentHandler = (todo) => {
-    setUserData(todo);
-  };
-
   const deleteHandler = (id) => {
     dispatch(__deleteTodos(id));
+  };
+
+  const ComentHandler = (todo) => {
+    setUserData(todo);
   };
 
   useEffect(() => {
     dispatch(__getTodos());
   }, []);
-
   return (
-    <Wrap>
-      <Container>
-        <Nav>
-          <Logo>
-            Todo<span style={{ color: "#000" }}>List</span>
-          </Logo>
-          <Update></Update>
-        </Nav>
-        <Section>
-          <SearchWrap>
-            <Search type="text" placeholder="검색어를 입력하세요." />
-            <SearchButton style={{ color: "#fff" }}>
-              <img
-                style={{ width: "48%", marginRight: "5px" }}
-                src="images/search.png"
-              />
-            </SearchButton>
-          </SearchWrap>
-          <div>
-            {todos.length == 0 ? (
-              <p style={{ color: "#777777", textAlign: "center" }}>
-                Todo List를 추가해주세요.
-              </p>
-            ) : (
-              todos.map((todo) => (
-                <Card>
-                  <ImgWrap
-                    style={{ backgroundImage: "url(" + todo.img + ")" }}
-                    onClick={() => {}}
-                  ></ImgWrap>
-                  <TextWrap>
-                    <Title>{todo.title}</Title>
-                    <p style={{ fontSize: "14px", marginBottom: "5px" }}>
-                      {todo.body}
-                    </p>
-                    <span style={{ fontSize: "14px" }}>{todo.username}</span> /{" "}
-                    <Time style={{ fontSize: "14px" }}>
-                      {timeForToday(todo.time)}
-                    </Time>
-                  </TextWrap>
-                  <DeleteButton onClick={() => deleteHandler(todo.id)}>
-                    <img style={{ width: "100%" }} src="images/trash.png" />
-                  </DeleteButton>
-                  <button onClick={() => ComentHandler(todo)}>댓글</button>
-                </Card>
-              ))
-            )}
-          </div>
-        </Section>
-        <Info>
-          <Comment key={todos.id} userData={userData} />
-        </Info>
-      </Container>
-      <Addbutton
-        onClick={() => {
-          navigate("/Update");
-        }}
-      >
-        Add
-      </Addbutton>
-    </Wrap>
+    <body>
+      <Wrap>
+        <Container>
+          <Nav>
+            <Logo>
+              <LogoSpan>Board List</LogoSpan>
+              {/* <LogoSpan>List</LogoSpan> */}
+            </Logo>
+            <Update></Update>
+          </Nav>
+          <Section>
+            <SearchWrap>
+              <Search type="text" placeholder="검색어를 입력하세요." />
+              <SearchButton style={{ color: "#fff" }}>
+                <img
+                  style={{ width: "48%", marginRight: "5px" }}
+                  src="images/search.png"
+                />
+              </SearchButton>
+            </SearchWrap>
+            <div>
+              {todos.length == 0 ? (
+                <p style={{ color: "#777777", textAlign: "center" }}>
+                  Todo List를 추가해주세요.
+                </p>
+              ) : (
+                todos.map((todo) => (
+                  <Card key={todo.id}>
+                    <ImgWrap
+                      style={{
+                        backgroundImage: "url(" + todo.img + ")",
+                      }}
+                    ></ImgWrap>
+                    <TextWrap
+                      onClick={() => {
+                        navigate(`/deatail/${todo.id}`);
+                      }}
+                    >
+                      <Title>{todo.title}</Title>
+                      <Body style={{ fontSize: "14px", marginBottom: "5px" }}>
+                        {todo.body}
+                      </Body>
+                      <span style={{ fontSize: "14px" }}>{todo.username}</span>{" "}
+                      /{" "}
+                      <Time style={{ fontSize: "14px" }}>
+                        {timeForToday(todo.time)}
+                      </Time>
+                    </TextWrap>
+
+                    <ButtonContainer>
+                      <DeleteButton>
+                        <img
+                          style={{ width: "100%" }}
+                          src="images/trash.png"
+                          onClick={() => deleteHandler(todo.id)}
+                        />
+                      </DeleteButton>
+                      <ChatButton>
+                        <img
+                          style={{ width: "100%" }}
+                          src="images/coment.png"
+                          onClick={() => ComentHandler(todo)}
+                        />
+                      </ChatButton>
+                    </ButtonContainer>
+                  </Card>
+                ))
+              )}
+            </div>
+          </Section>
+          <Info>
+            <Comment key={todos.id} userData={userData} />
+          </Info>
+        </Container>
+      </Wrap>
+    </body>
   );
 };
 
@@ -96,7 +114,25 @@ const Wrap = styled.div`
   max-width: 100vw;
   height: 100vh;
   display: flex;
-  background-color: #dfe4f5;
+  background: -moz-linear-gradient(
+    -45deg,
+    #183850 0,
+    #183850 25%,
+    #192c46 50%,
+    #22254c 75%,
+    #22254c 100%
+  );
+  background: -webkit-linear-gradient(
+    -45deg,
+    #183850 0,
+    #183850 25%,
+    #192c46 50%,
+    #22254c 75%,
+    #22254c 100%
+  );
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  /* background-color: #dfe4f5; */
   position: relative;
 `;
 
@@ -109,17 +145,52 @@ const Container = styled.section`
   display: flex;
   box-shadow: 0 1px 6px 0 rgba(32, 33, 36, 0.28);
 `;
+
 const Nav = styled.nav`
   width: 350px;
   height: 100%;
   padding: 48px;
 `;
+
+const typing = keyframes`
+  0% {
+    width: 0%;
+  }
+  50% {
+    width: 100%;
+
+  }
+  100% {
+    width: 100%;
+    border-right: none;
+  }
+`;
+
 const Logo = styled.h1`
   font-family: "Noto Sans KR", sans-serif;
   font-size: 30px;
   font-weight: bold;
   color: #5060ff;
+  display: flex;
+  position: relative;
 `;
+
+const LogoSpan = styled.span`
+  color: transparent;
+  white-space: nowrap;
+  &::before {
+    content: "Board List";
+    position: absolute;
+    color: #5060ff;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    border-right: 1px solid black;
+    animation: ${typing} 5s steps(31) forwards;
+  }
+`;
+
 const Section = styled.div`
   padding-top: 48px;
   width: 680px;
@@ -164,6 +235,7 @@ const Card = styled.div`
   border-radius: 10px;
   background-color: #fff;
   display: flex;
+  justify-content: space-between;
   padding: 20px;
   margin: 0 auto;
   gap: 20px;
@@ -176,10 +248,16 @@ const ImgWrap = styled.div`
   border-radius: 5px;
   background-size: cover;
   background-position: center center;
+  box-shadow: 2px 4px 2px 2px grey;
 `;
 
 const TextWrap = styled.div`
+  width: 100%;
   cursor: pointer;
+  box-shadow: 2px 4px 2px 2px grey;
+  border-radius: 10px;
+  padding: 0px 5px 0px 5px;
+  border: 1px solid black;
 `;
 
 const Title = styled.h5`
@@ -187,32 +265,63 @@ const Title = styled.h5`
   font-weight: 500;
   line-height: 25px;
 `;
+
+const Body = styled.p`
+  width: 300px;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+`;
 const Time = styled.span`
   text-align: right;
 `;
 
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-direction: column;
+  gap: 10px;
+`;
+
 const DeleteButton = styled.button`
-  width: 15px;
-  height: 15px;
-  margin-left: auto;
+  width: 20px;
+  background: transparent;
   padding: 0;
   border: none;
   cursor: pointer;
+  box-shadow: 2px 2px 2px 2px grey;
+  &:hover {
+    transform: rotate(30deg);
+    transition-duration: 0.5s;
+    transition-timing-function: ease-in;
+  }
+  /* animation-duration: 2s; */
 `;
 
-const Addbutton = styled.button`
-  width: 170px;
-  height: 55px;
+const ChatButton = styled.button`
+  width: 25px;
+  background: transparent;
+  margin-left: auto;
+  padding: 0;
   border: none;
-  font-size: 16px;
+  border-radius: 10px;
+  box-shadow: 2px 2px 2px 2px grey;
+  cursor: pointer;
+  &:hover {
+    transform: rotateY(180deg);
+    transition-duration: 0.5s;
+  }
+`;
+
+const ComentsInput = styled.input`
+  border-bottom: 1px solid #7884fb;
+`;
+
+const ComentsButton = styled.button`
   background-color: #7884fb;
   color: #fff;
-  position: fixed;
-  border-radius: 20px;
-  right: 10px;
-  bottom: 10px;
-  box-shadow: 0px 2px 20px #a7a7a7;
-  cursor: pointer;
+  padding: 0px 5px 0px 5px;
+  border-radius: 5px;
 `;
 
 export default Home;
